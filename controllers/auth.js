aa48c6f35f8a4053d95606a313ac222815a52d61
Mridu-devs <mridu.devs@gmail.com>
2023-04-23 21:16:37 +0530

    user registration api created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,35 @@
 import { db } from "../connect.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 export const register = (req, res) => {
   //Check if user exists
 
-  const q = "SELECT FROM users WHERE username = ?"
+  const q = "SELECT * FROM users WHERE username = ?";
 
-  db.query(q, [req.body.username],(err,data)=>{
-    if(err) return res.status(500).json(err)
-    if(data.length) return res.status(409).json("User already exists!")
-  })
-
-  // Create a New Userss
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+    // Create a New Userss
     //Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q =
+      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
+    });
+  });
 };
+
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
