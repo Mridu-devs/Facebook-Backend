69d6ef685ec8486c314882d9e57f9c63cdedd8d5
Mridu-devs <mridu.devs@gmail.com>
2023-04-24 14:54:23 +0530

    login&out api created,added JWT token

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 //middleware
-app.use(express.json())
-
-
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
